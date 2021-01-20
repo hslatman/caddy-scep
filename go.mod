20236266605d5ca07e8b5d72c7766ef194437e9e
Herman Slatman <hermanslatman@hotmail.com>
2021-01-20 10:43:29 +0100

    Add initial integration with micromdm/scep

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/hslatman/caddy-scep
 
 go 1.15
 
-require github.com/caddyserver/caddy/v2 v2.3.0
+require (
+	github.com/caddyserver/caddy/v2 v2.3.0
+	github.com/micromdm/scep v1.0.0
+	go.uber.org/zap v1.16.0
+)
--- pkg/scep/scep.go
+++ pkg/scep/scep.go
@@ -15,10 +15,19 @@
 package scep
 
 import (
+	"crypto/rsa"
+	"crypto/x509"
+	"fmt"
+	"math/big"
 	"net/http"
 
 	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/caddyserver/caddy/v2/modules/caddypki"
+	"go.uber.org/zap"
+
+	_ "github.com/micromdm/scep/depot"
+	scepserver "github.com/micromdm/scep/server"
 )
 
 func init() {
@@ -27,6 +36,7 @@ func init() {
 
 // Handler is a SCEP server handler
 type Handler struct {
+	logger *zap.Logger
 }
 
 // CaddyModule returns the Caddy module information.
@@ -39,6 +49,57 @@ func (Handler) CaddyModule() caddy.ModuleInfo {
 
 // Provision sets up the ACME server handler.
 func (h *Handler) Provision(ctx caddy.Context) error {
+	h.logger = ctx.Logger(h)
+
+	pkiModule, err := ctx.App("pki")
+	if err != nil {
+		return err
+	}
+
+	caLocal := "local"
+
+	pkiApp := pkiModule.(*caddypki.PKI)
+	fmt.Println(pkiApp)
+	fmt.Println(fmt.Sprintf("%#+v", pkiApp.CAs))
+	ca, ok := pkiApp.CAs[caLocal]
+	if !ok {
+		return fmt.Errorf("no certificate authority configured with id: %s", caLocal)
+	}
+
+	fmt.Println(ca)
+
+	depot, err := NewCaddyDepot(ca)
+	if err != nil {
+		return err
+	}
+
+	svcOptions := []scepserver.ServiceOption{
+		// scepserver.ChallengePassword(*flChallengePassword),
+		// scepserver.WithCSRVerifier(csrVerifier),
+		// scepserver.CAKeyPassword([]byte(*flCAPass)),
+		scepserver.AllowRenewal(14),   // 14 days
+		scepserver.ClientValidity(60), // 60 days
+		// scepserver.WithLogger(logger),
+	}
+
+	fmt.Println(svcOptions)
+
+	svc, err := scepserver.NewService(depot, svcOptions...)
+	if err != nil {
+		h.logger.Error(err.Error())
+		return err
+	}
+
+	fmt.Println(svc)
+	//svc = scepserver.NewLoggingService(log.With(lginfo, "component", "scep_service"), svc)
+
+	// TODO: link scepserver to the pki (and thus storage)
+	// TODO: add routes (and serve them)
+	// TODO: how does auth work with the scepserver? ChallengePassword? Something different?
+	// TODO: implement according to RFC
+	// TODO: add tests
+	// TODO: ensure smaller dependency stack (i.e. no go-kit if it's not necessary; might result in providing more scep of our own)
+
 	return nil
 }
 
@@ -61,6 +122,32 @@ func (h Handler) Cleanup() error {
 	// 	ash.logger.Error("closing CA database", zap.String("db_key", key), zap.Error(err))
 	// }
 	// return err
+	return nil
+}
+
+type CaddyDepot struct {
+	ca *caddypki.CA
+}
+
+func NewCaddyDepot(ca *caddypki.CA) (*CaddyDepot, error) {
+	return &CaddyDepot{
+		ca: ca,
+	}, nil
+}
+
+func (cd *CaddyDepot) CA(pass []byte) ([]*x509.Certificate, *rsa.PrivateKey, error) {
+	return nil, nil, nil
+}
+
+func (cd *CaddyDepot) Put(name string, crt *x509.Certificate) error {
+	return nil
+}
+
+func (cd *CaddyDepot) Serial() (*big.Int, error) {
+	return nil, nil
+}
+
+func (cd *CaddyDepot) HasCN(cn string, allowTime int, cert *x509.Certificate, revokeOldCertificate bool) error {
 	return nil
 }
 
